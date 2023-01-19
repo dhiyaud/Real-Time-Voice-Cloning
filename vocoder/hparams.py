634729f73ee8e75d81a691152452ed7873382ab1
Dhiya Ulhaq Dewangga <dhiya.dewangga@prosa.ai>
2023-01-19 09:36:58 +0900

    Update params

--- vocoder/hparams.py
+++ vocoder/hparams.py
@@ -23,7 +23,7 @@ mu_law = True                       # Recommended to suppress noise if using raw
 # WAVERNN / VOCODER --------------------------------------------------------------------------------
 voc_mode = 'MOL'                    # either 'RAW' (softmax on raw bits) or 'MOL' (sample from 
 # mixture of logistics)
-voc_upsample_factors = (5, 5, 11)    # NB - this needs to correctly factorise hop_length
+voc_upsample_factors = (5, 5, 8)    # NB - this needs to correctly factorise hop_length
 voc_rnn_dims = 512
 voc_fc_dims = 512
 voc_compute_dims = 128
